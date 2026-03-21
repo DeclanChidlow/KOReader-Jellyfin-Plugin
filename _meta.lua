2e662fd3318422db5d65e4d6a8da2e7f0c116d44
Declan Chidlow <84255570+DeclanChidlow@users.noreply.github.com>
2026-03-21 12:28:30 +0800

    chore: bump version in meta

--- _meta.lua
+++ _meta.lua
@@ -3,7 +3,7 @@ return {
 	name = "jellyfin",
 	fullname = _("Jellyfin"),
 	description = _([[Download books from your Jellyfin server and mark them as read.]]),
-	version = "1.0.0",
+	version = "1.1.0",
 	author = "Declan Chidlow (Vale)",
 	url = "https://github.com/DeclanChidlow/KOReader-Jellyfin-Plugin",
 }
